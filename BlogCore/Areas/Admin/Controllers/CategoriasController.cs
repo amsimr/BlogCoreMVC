2d6b4cb43869b8966a8d54dcdbd459a3bd579b6a
amsimr <amsi.medinadr@gmail.com>
2022-10-22 00:12:42 -0400

    Creacion CategoriaController

---
+++ BlogCore/Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,41 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using BlogCore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+        private readonly ApplicationDbContext _context;
+
+        public CategoriasController(IContenedorTrabajo contenedorTrabajo, ApplicationDbContext context)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        #region
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Opcion 1
+            return Json(new { data = _contenedorTrabajo.Categoria.GetAll() });
+        }
+
+
+        #endregion
+
+
+    }
+}
